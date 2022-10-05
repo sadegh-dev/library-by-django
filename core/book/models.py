656bfee235a9ab42cadebf7d5b0a17494cdf152e
sadegh-dev <sadegh.rez@gmail.com>
2022-10-05 12:27:35 +0330

    basic class models

--- core/book/models.py
+++ core/book/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=300)
+    #date_birth = models.
+    
+
+
+class Publisher(models.Model):
+    pass 
+
+
+class Book(models.Model):
+    pass
