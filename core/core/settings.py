7204e6bcae2ec2b60e9b6f2585fba96fbac20a0f
sadegh-dev <sadegh.rez@gmail.com>
2023-06-19 15:05:20 +0330

    mkdir files [media and static]ans set settings file and url file

--- core/core/settings.py
+++ core/core/settings.py
@@ -116,6 +116,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/'),
+
+
+# Media files (Pictures, Videos)
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
